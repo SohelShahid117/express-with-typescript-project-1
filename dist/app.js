2504f76766f77c985764a16ebf12566ee53a4ed0
SohelShahid117 <sohelshahid09@gmail.com>
2025-03-06 00:23:52 +0600

    done 7-8 Middleware in express.js

--- dist/app.js
+++ dist/app.js
@@ -5,18 +5,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express')
 const express_1 = __importDefault(require("express"));
+// import { userInfo } from "os"
 const app = (0, express_1.default)();
 // const port = 3000
 //parser
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get('/', (req, res) => {
+//middleware
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+// app.get('/', (req:Request, res:Response) => {
+// app.get('/:userId', (req:Request, res:Response) => {
+// app.get('/:userId/:subId', (req:Request, res:Response) => {
+// app.get('/', (req:Request, res:Response) => {
+app.get('/', logger, (req, res) => {
+    // console.log(req.params)
+    // console.log(req.query)
+    console.log(req.query.name, req.query.email);
     // res.send('Hello World!')
     // res.send('Hello programmer!')
     // res.send('Hello developers!')
     res.send('Hello Problem solvers!');
 });
-app.post("/", (req, res) => {
+app.post("/", logger, (req, res) => {
     console.log("i m in level2 developer");
     res.send("i m in level2 developer");
     console.log(req.body);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 // const express = require('express')
-import express from "express"
+import express, { NextFunction } from "express"
+import { hostname } from "os"
+// import { userInfo } from "os"
 const app = express()
 // const port = 3000
 
@@ -7,14 +9,27 @@ const app = express()
 app.use(express.json())
 app.use(express.text())
 
-app.get('/', (req:Request, res:Response) => {
+//middleware
+const logger = (req:Request, res:Response,next:NextFunction)=>{
+  console.log(req.url,req.method,req.hostname)
+  next()
+}
+
+// app.get('/', (req:Request, res:Response) => {
+// app.get('/:userId', (req:Request, res:Response) => {
+// app.get('/:userId/:subId', (req:Request, res:Response) => {
+// app.get('/', (req:Request, res:Response) => {
+app.get('/',logger, (req:Request, res:Response) => {
+  // console.log(req.params)
+  // console.log(req.query)
+  console.log(req.query.name,req.query.email)
   // res.send('Hello World!')
   // res.send('Hello programmer!')
   // res.send('Hello developers!')
   res.send('Hello Problem solvers!')
 })
 
-app.post("/",(req:Request,res:Response)=>{
+app.post("/",logger,(req:Request,res:Response)=>{
   console.log("i m in level2 developer")
   res.send("i m in level2 developer")
   console.log(req.body)
