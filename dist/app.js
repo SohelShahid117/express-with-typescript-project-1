c1f7b134c1d9bebba64bea4c22bbda85380a74d7
SohelShahid117 <sohelshahid09@gmail.com>
2025-03-06 21:44:38 +0600

    done 7-9 Routing in express.js

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,33 @@ const app = (0, express_1.default)();
 //parser
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+const userRouter = express_1.default.Router();
+console.log(userRouter);
+// app.use("/",userRouter)
+app.use("/api/v1/users/", userRouter);
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/course/", courseRouter);
+// userRouter.get(
+userRouter.post(
+// "/api/v1/users/create-user",
+"/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "user is created successfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "course is created successfully",
+        data: course,
+    });
+});
 //middleware
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
@@ -20,14 +47,14 @@ const logger = (req, res, next) => {
 // app.get('/:userId', (req:Request, res:Response) => {
 // app.get('/:userId/:subId', (req:Request, res:Response) => {
 // app.get('/', (req:Request, res:Response) => {
-app.get('/', logger, (req, res) => {
+app.get("/", logger, (req, res) => {
     // console.log(req.params)
     // console.log(req.query)
     console.log(req.query.name, req.query.email);
     // res.send('Hello World!')
     // res.send('Hello programmer!')
-    // res.send('Hello developers!')
-    res.send('Hello Problem solvers!');
+    res.send("Hello developers!");
+    // res.send('Hello Problem solvers!')
 });
 app.post("/", logger, (req, res) => {
     console.log("i m in level2 developer");
--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,73 @@
 // const express = require('express')
-import express, { NextFunction } from "express"
-import { hostname } from "os"
+import express, { NextFunction } from "express";
+import { hostname } from "os";
 // import { userInfo } from "os"
-const app = express()
+const app = express();
 // const port = 3000
 
 //parser
-app.use(express.json())
-app.use(express.text())
+app.use(express.json());
+app.use(express.text());
+
+const userRouter = express.Router();
+console.log(userRouter);
+// app.use("/",userRouter)
+app.use("/api/v1/users/", userRouter);
+
+const courseRouter = express.Router();
+app.use("/api/v1/course/", courseRouter);
+
+// userRouter.get(
+userRouter.post(
+  // "/api/v1/users/create-user",
+  "/create-user",
+  (req: Request, res: Response) => {
+    const user = req.body;
+    console.log(user);
+
+    res.json({
+      success: true,
+      message: "user is created successfully",
+      data: user,
+    });
+  }
+);
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "course is created successfully",
+    data: course,
+  });
+});
 
 //middleware
-const logger = (req:Request, res:Response,next:NextFunction)=>{
-  console.log(req.url,req.method,req.hostname)
-  next()
-}
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+};
 
 // app.get('/', (req:Request, res:Response) => {
 // app.get('/:userId', (req:Request, res:Response) => {
 // app.get('/:userId/:subId', (req:Request, res:Response) => {
 // app.get('/', (req:Request, res:Response) => {
-app.get('/',logger, (req:Request, res:Response) => {
+app.get("/", logger, (req: Request, res: Response) => {
   // console.log(req.params)
   // console.log(req.query)
-  console.log(req.query.name,req.query.email)
+  console.log(req.query.name, req.query.email);
   // res.send('Hello World!')
   // res.send('Hello programmer!')
-  // res.send('Hello developers!')
-  res.send('Hello Problem solvers!')
-})
+  res.send("Hello developers!");
+  // res.send('Hello Problem solvers!')
+});
 
-app.post("/",logger,(req:Request,res:Response)=>{
-  console.log("i m in level2 developer")
-  res.send("i m in level2 developer")
-  console.log(req.body)
-})
+app.post("/", logger, (req: Request, res: Response) => {
+  console.log("i m in level2 developer");
+  res.send("i m in level2 developer");
+  console.log(req.body);
+});
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
